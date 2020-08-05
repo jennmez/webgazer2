7a5fed70fc83e3c7ba8399e322cc0f79cc74fc10
jennmez <jennmez@gmail.com>
2020-08-05 11:05:30 -0500

    Updates

---
+++ precache-manifest.4d24b9fdbc46b5cce9c1500a3f14af2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "870f3284bb62bc8bd9331cdb7f2198eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac215b9f033dbe60369b",
+    "url": "/static/css/main.75df9f48.chunk.css"
+  },
+  {
+    "revision": "75297f235858ffd564fe",
+    "url": "/static/js/2.b12964f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b12964f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac215b9f033dbe60369b",
+    "url": "/static/js/main.b31d3a16.chunk.js"
+  },
+  {
+    "revision": "a65b140cb20056a9e0af",
+    "url": "/static/js/runtime-main.00e2f38c.js"
+  },
+  {
+    "revision": "cf18b6297c3065e52d11afe04a8b6110",
+    "url": "/static/media/mintbean.cf18b629.png"
+  }
+]);
